4c9f78c4dfd3b022e2b77508af088d92f4c0de60
Stanislav <wildkain@gmail.com>
2018-06-03 14:08:41 +0300

    Add depend(destroy), has_many shoulda test

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  it {should validate_presence_of :body}
   it { should belong_to(:question) }
+  it {should validate_presence_of :body}
 end
