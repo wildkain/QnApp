5db2c7f33fd7ddc861d8eb8cb8dc6f6decc895ea
Stanislav <wildkain@gmail.com>
2018-06-05 14:18:39 +0300

    rubocop

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,21 +1,19 @@
+# frozen_string_literal: true
+
 class QuestionsController < ApplicationController
-  before_action :load_question, only: %i[ show edit update destroy ]
+  before_action :load_question, only: %i[show edit update destroy]
 
   def index
     @questions = Question.all
   end
 
-  def show
-
-  end
+  def show; end
 
   def new
     @question = Question.new
   end
 
-  def edit
-
-  end
+  def edit; end
 
   def create
     @question = Question.new(question_params)
