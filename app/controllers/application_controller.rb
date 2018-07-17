80410984254f4cfbdd34b531516359a9272ebab3
Stanislav <wildkain@gmail.com>
2018-07-18 02:13:10 +0300

    Ability tests, authorize for all resources

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,11 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exeption|
     redirect_to root_url, alert: exeption.message
   end
+  check_authorization
   before_action :gon_user, unless: :devise_controller?
 
   def gon_user
     gon.user_id = current_user.id if current_user
   end
+
 end
--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -10,6 +10,33 @@ RSpec.describe Ability, type: :model do
     it { should be_able_to :read, Comment }
 
     it { should_not be_able_to :manage, :all }
+  end
 
+  describe 'for Admin' do
+    let(:user) { create(:user, admin: true)}
+    it { should be_able_to :manage, :all}
+  end
+
+  describe 'for user' do
+    let(:user) { create :user }
+    let(:other_user) { create :user }
+    let(:question) { create :question}
+
+
+    it { should_not be_able_to :manage, :all}
+    it { should be_able_to :read, :all }
+
+    it { should be_able_to :create, Question }
+    it { should be_able_to :create, Answer }
+    it { should be_able_to :create, Comment }
+
+    it { should be_able_to :update, create(:question, user: user), user: user }
+    it { should_not be_able_to :update, create(:question, user: other_user), user: user }
+
+    it { should be_able_to :update, create(:answer, user: user), user: user }
+    it { should_not be_able_to :update, create(:answer, user: other_user), user: user }
+
+    it { should be_able_to :update, create(:comment, commentable: question, user: user), user: user }
+    it { should_not be_able_to :update, create(:comment, commentable: question, user: other_user), user: user }
   end
 end
