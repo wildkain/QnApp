4ba5b405a9abb3de41f495868695c05c751748d9
Stanislav <wildkain@gmail.com>
2018-07-21 18:19:01 +0300

    Show single question api

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,7 +1,18 @@
 class Api::V1::QuestionsController < Api::V1::BaseController
+  before_action :find_question, only: :show
 
   def index
     @questions = Question.all
-    respond_with @questions
+    respond_with @questions, each_serializer: QuestionCollectionSerializer
+  end
+
+  def show
+    respond_with @question
+  end
+
+  private
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 end
--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -59,4 +59,41 @@ describe 'Questions API' do
 
   end
 
+
+  describe 'GET /show' do
+    let(:question) { create :question }
+
+    context 'unauthorized' do
+      it 'returns 401 status if there is no access_token' do
+        get "/api/v1/questions/#{question.id}", params: { format: :json }
+        expect(response.status).to eq 401
+      end
+
+      it 'returns 401 status if access_token is invalid' do
+        get "/api/v1/questions/#{question.id}", params: { format: :json, access_token: '1234'}
+        expect(response.status).to eq 401
+      end
+
+    end
+
+    context 'authorized' do
+      let(:user) { create :user }
+      let(:access_token) { create(:access_token, resource_owner_id: user.id) }
+      let!(:answer) {create(:answer, question: question )}
+      before { get "/api/v1/questions/#{question.id}", params: { format: :json, access_token: access_token.token } }
+
+      it 'returns status 200 OK' do
+        expect(response).to be_successful
+      end
+
+      %w(id title body created_at updated_at).each do |attr|
+        it "question object contains #{attr}" do
+          expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path("#{attr}")
+        end
+      end
+    end
+  end
+
+
+
 end
