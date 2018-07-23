ddb6117b7acfacad79cd156401d9000dc431008e
Stanislav <wildkain@gmail.com>
2018-07-23 11:00:13 +0300

    Alias current_user to use current_resource_owner

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -9,4 +9,6 @@ class Api::V1::BaseController < ApplicationController
   def current_resource_owner
     @current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
   end
+
+  alias :current_user :current_resource_owner
 end
