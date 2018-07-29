e8400d44de70551f3f05ed8a4ab2ce84de5e2409
Stanislav <wildkain@gmail.com>
2018-07-29 22:10:29 +0300

    Try devise fix6

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -257,8 +257,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :vkontakte, ENV["VKONTAKE_APP_ID"], ENV["VKONTAKTE_APP_SECRET"]
-  config.omniauth :twitter, ENV["TWITTER_APP_ID"], ENV["TWITTER_APP_SECRET"]
+  config.omniauth :vkontakte, Rails.application.credentials.vkontakte[:vkontakte_app_id], Rails.application.credentials.vkontakte[:vkontakte_app_secret], scope: [:email]
+  config.omniauth :twitter, Rails.application.credentials.twitter[:twitter_app_id], Rails.application.credentials.twitter[:twitter_app_secret], scope: [:email]
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
