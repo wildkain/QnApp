02d3c8e97e5f755cc2181f5b28a908bb0a889785
Stanislav <wildkain@gmail.com>
2018-08-04 17:10:34 +0300

    Add username to users. Info block for resurces

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe User, type: :model do
   it { should have_many(:answers).dependent(:destroy) }
   it { should have_many(:subscriptions).dependent(:destroy) }
   it { should have_many(:comments) }
+  it { should validate_presence_of :first_name}
+  it { should validate_presence_of :last_name}
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
 
